840c2e657f98017cfacd202b90047dfcd02e97ed
Laurent Granié <lgranie@users.noreply.github.com>
2019-12-02 15:26:02 +0100

    Init keymap.c

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,33 +1,66 @@
-/* Copyright 2019 mechmerlin
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_UP,   KC_DOT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,                 KC_SPC,              MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
-    ),
-    [1] = LAYOUT(
-        RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______,   BL_TOGG, BL_DEC,  BL_INC,  BL_STEP, _______, _______, _______, _______, _______, _______,
-        _______,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______,
-        _______, _______, _______, _______,                _______,             _______, _______, _______, _______
-    ),
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+enum layer_names {
+  _BL,
+  _FL,
+  _FR
+};
 
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* Keymap _BL: (Base Layer) Default Layer
+   * ,-----------------------------------------------.
+   * |Esc| Q | W | E | R | T | Y | U | I | O | P | BS|
+   * |-----------------------------------------------|
+   * |Tab | A | S | D | F | G | H | J | K | L | Enter|
+   * |-----------------------------------------------|
+   * | Shift | Z | X | C | V | B | N | M | , | . | / |
+   * |-----------------------------------------------|
+   * |Ctl|GUI|Alt| Space FL |Space FR|   |   |   |   |
+   * `-----------------------------------------------'
+   */
+    [_BL] = LAYOUT(
+       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
+           KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,       KC_ENT,
+               KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
+      KC_LCTL, KC_LGUI, KC_LALT,       LT(_FL, KC_SPC),     LT(_FR, KC_SPC),  _______, _______, _______, _______
+    ),
+  /* Keymap _FL: (Function Left Layer) Left Layer
+   * ,-----------------------------------------------.
+   * | ~ | ! | @ | # | $ | % | ^ | & | * | ( | ) |   |
+   * |-----------------------------------------------|
+   * |    |F1 |F2 |F3 |   |   |   | { | } | + |      |
+   * |-----------------------------------------------|
+   * |       |F4 |F5 |F6 |Del|End|   | ; | " | | | - |
+   * |-----------------------------------------------|
+   * |   |   |   |          |        |   |   |   |   |
+   * `-----------------------------------------------'
+   */
+    [_FL] = LAYOUT(
+      KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+          _______,   KC_F1,   KC_F2,   KC_F3, BL_STEP, _______, _______, KC_LCBR, KC_RCBR, _______,      _______,
+               _______,   KC_F4,   KC_F5,   KC_F6, RGB_HUD, RGB_SAI, RGB_SAD, KC_SCLN, KC_DQUO, KC_PIPE, KC_MINS,
+      _______, _______, _______,               _______,             _______,  _______, _______, _______, _______
+    ),
+  /* Keymap _FR: (Function Right Layer) Right Layer
+   * ,-----------------------------------------------.
+   * | ` | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 |   |
+   * |-----------------------------------------------|
+   * |    |F7 |F8 |F9 |   |   |   | [ | ] | = |      |
+   * |-----------------------------------------------|
+   * |       |F10|F11|F12|   |   |   | : | ' | \ | _ |
+   * |-----------------------------------------------|
+   * |   |   |   |          |        |   |   |   |   |
+   * `-----------------------------------------------'
+   */
+    [_FR] = LAYOUT(
+       KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
+          _______,   KC_F7,   KC_F8,   KC_F9, _______, _______, _______, KC_LBRC, KC_RBRC, KC_EQL,       _______,
+               _______,  KC_F10,  KC_F11,  KC_F12, _______, _______, _______, KC_COLN, KC_QUOT, KC_BSLS, KC_UNDS,
+      _______, _______, _______,               _______,             _______,  _______, _______, _______, _______
+    )
 };
 
