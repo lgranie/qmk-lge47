1cb623339cf01cf35fb5e277feb72b83bbaac14e
Laurent Granié <lgranie@users.noreply.github.com>
2020-01-19 18:44:26 +0100

    Update cols and rows

--- config.h
+++ config.h
@@ -41,12 +41,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_COL_PINS { F5, F6, F7, F0, F1, F4, B6, B5, D7, C7, D6, B7 }
-#define MATRIX_ROW_PINS { D2, D3, D0, D1 }
+#define MATRIX_COL_PINS { F7, F6, F5, F4, F1, F0, D7, B5, B6, B7, D6, C7 }
+#define MATRIX_ROW_PINS { D0, D1, D2, D3 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
-#define DIODE_DIRECTION COL2ROW
+#define DIODE_DIRECTION ROW2COL
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
