637fddf8627db5327e67b46eb54339f91e543aa3
lgranie <laurent.granie@gmail.com>
2020-01-26 10:59:14 +0100

    Fix slsh comm dot order

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------|
    * |Tab | A | S | D | F | G | H | J | K | L | Enter|
    * |-----------------------------------------------|
-   * | Shift | Z | X | C | V | B | N | M | , | . | / |
+   * | Shift | Z | X | C | V | B | N | M | / | , | . |
    * |-----------------------------------------------|
    * |Ctl|GUI|Alt| Space FL  |   Space FR|   |   |   |
    * `-----------------------------------------------'
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT(
       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
       KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,       KC_ENT,
-      KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
+      KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_SLSH, KC_COMM, KC_DOT,
       KC_LCTL, KC_LGUI, KC_LALT,       LT(_FL, KC_SPC),     LT(_FR, KC_SPC),  _______, _______, _______
     ),
   /* Keymap _FL: (Function Left Layer) Left Layer
