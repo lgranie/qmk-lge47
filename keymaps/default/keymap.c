aae6cde0a11c2dde9b62e2f40f5215d6bfab2fda
lgranie <laurent.granie@gmail.com>
2020-06-25 17:13:24 +0200

    reorder comm keys

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------|
    * |Tab | A | S | D | F | G | H | J | K | L | Enter|
    * |-----------------------------------------------|
-   * | Shift | Z | X | C | V | B | N | M | / | , | . |
+   * | Shift | Z | X | C | V | B | N | M | , | . | / |
    * |-----------------------------------------------|
    * |Ctl|GUI|Alt|     FL    | Space FR  |Lft|Dwn|Rgt|
    * `-----------------------------------------------'
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT(
       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
       KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,       KC_ENT,
-      KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_SLSH, KC_COMM, KC_DOT,
+      KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM, KC_DOT, KC_SLSH,
       KC_LCTL, KC_LGUI, KC_LALT,          MO(_FL),     LT(_FR, KC_SPC),  KC_LEFT, KC_DOWN, KC_RGHT
     ),
   /* Keymap _FL: (Function Left Layer) Left Layer
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------|
    * |    |F1 |F2 |F3 |   |   |   | [ | ] | = |      |
    * |-----------------------------------------------|
-   * |       |F4 |F5 |F6 |Del|End|   | ; | ' | \ | - |
+   * |       |F4 |F5 |F6 |Del|End|   | ; | \ | - | ' |
    * |-----------------------------------------------|
    * |   |   |   |          |        |   |   | Up|   |
    * `-----------------------------------------------'
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FL] = LAYOUT(
       KC_GRV,    KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,   KC_9,  KC_0,  KC_DEL,
       _______,   KC_F1,   KC_F2,   KC_F3, _______, _______, _______, KC_LBRC, KC_RBRC, KC_EQL,      _______,
-      _______,   KC_F4,   KC_F5,   KC_F6, KC_DEL, KC_END, _______, KC_SCLN, KC_QUOT, KC_BSLS, KC_MINS,
+      _______,   KC_F4,   KC_F5,   KC_F6, KC_DEL, KC_END, _______, KC_SCLN, KC_BSLS, KC_MINS, KC_QUOT,
       _______, _______, _______,               _______,             _______,  _______, KC_UP, _______
     ),
   /* Keymap _FR: (Function Right Layer) Right Layer
