ca51ae1d38288b03adcc73f4b7bfd2b532814168
Laurent Granié <laurent.granie@gmail.com>
2021-05-05 21:17:16 +0200

    SWitch Ent/MO_FR & SFft/___

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -15,18 +15,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------.
    * |Esc| Q | W | E | R | T | Y | U | I | O | P | BS|
    * |-----------------------------------------------|
-   * |Tab| A | S | D | F | G | H | J | K | L | ; |Ent|
+   * |Tab| A | S | D | F | G | H | J | K | L | ; |Fn1|
    * |-----------------------------------------------|
-   * |Sft| Z | X | C | V | B | N | M | , | . | / |Sft|
+   * |   | Z | X | C | V | B | N | M | , | . | / |Sft|
    * |-----------------------------------------------|
-   * |Ctl|GUI|Alt|   |Fn0| Space |Fn1|Lft|Dwn| Up|Rgt|
+   * |Ctl|GUI|Alt|Sft|Fn0| Space |Ent|Lft|Dwn| Up|Rgt|
    * `-----------------------------------------------'
    */
     [_BL] = LAYOUT(
        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
-       KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LSFT,
-      KC_LCTL, KC_LGUI, KC_LALT, _______, MO(_FL),      KC_SPC,      MO(_FR), KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
+       KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  MO(_FR),
+      _______,    KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LSFT,
+      KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, MO(_FL),      KC_SPC,      KC_ENT, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
     ),
   /* Keymap _FL: (Function Left Layer) Left Layer
    * ,-----------------------------------------------.
