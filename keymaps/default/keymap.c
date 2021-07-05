9643c9a07e1de105f335b22a68616597b67cdb47
Laurent Granié <laurent.granie@gfi.fr>
2021-07-05 11:33:22 +0200

    Update keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -24,9 +24,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
     [_BL] = LAYOUT(
        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
-       KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  MO(_FR),
-      _______,    KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LSFT,
-      KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, MO(_FL),      KC_SPC,      KC_ENT, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
+       KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+      KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, MO(_FL),      KC_SPC,     MO(_FR), KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
     ),
   /* Keymap _FL: (Function Left Layer) Left Layer
    * ,-----------------------------------------------.
@@ -63,4 +63,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______, _______, _______, _______,      _______,     _______, _______, OUT_USB,   RESET,  OUT_BT
     )
 };
-
