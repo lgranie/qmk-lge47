163d1c9ad2042dd24ebe5e10a58ce257c8a61b7c
Laurent Granié <laurent.granie@gfi.fr>
2021-11-12 12:51:58 +0100

    use TO

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
        KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-      KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, MO(_FL),      KC_SPC,     MO(_FR), KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
+      KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, TO(_FL),      KC_SPC,     TO(_FR), KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
     ),
   /* Keymap _FL: (Function Left Layer) Left Layer
    * ,-----------------------------------------------.
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,
       _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_BSLS, KC_QUOT, _______, _______,
       _______, _______, _______, _______, _______, _______, KC_MINS,  KC_EQL, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______,      _______,     _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END
+      _______, _______, _______, _______,  TO(_BL),      _______,     _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END
     ),
   /* Keymap _FR: (Function Right Layer) Right Layer
    * ,-----------------------------------------------.
@@ -60,6 +60,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______,      _______,     _______, _______, OUT_USB,   RESET,  OUT_BT
+      _______, _______, _______, _______,  TO(_BL),      _______,     _______, _______, OUT_USB,   RESET,  OUT_BT
     )
 };
